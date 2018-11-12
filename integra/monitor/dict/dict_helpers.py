7108e0fe9548169e19d9b9fb0bb3051c4b1b774f
wgebis <wgebis@gmail.com>
2018-11-12 16:53:38 +0100

    Typos

--- integra/monitor/dict/dict_helpers.py
+++ integra/monitor/dict/dict_helpers.py
@@ -6,11 +6,11 @@ from integra.monitor.dict.slow import Event
 def get_event_category(k):
     switcher = {
         'A': 'ALARM',
-        'Z': 'ZA\xc5\xc4CZENIE CZUWANIA - wymagana interwencja obs\xc5ugi stacji',
-        'W': 'WY\xc5\xc4CZENIE CZUWANIA - nie wymaga interwencji, ster.wsk.czuwania',
+        'Z': 'ZAŁĄCZENIE CZUWANIA - wymagana interwencja obsługi stacji',
+        'W': 'WYŁĄCZENIE CZUWANIA - nie wymaga interwencji, ster.wsk.czuwania',
         'F': 'AWARIA - wymagana interwencja, ster.wsk.awarii',
         'N': 'KONIEC AWARII - nie wymaga interwencji, ster.wsk.awarii',
-        'T': 'TEST - interwencja przy braku kodu o okre\xc5lonym czasie',
+        'T': 'TEST - interwencja przy braku kodu o określonym czasie',
         'U': 'UWAGA - sygnalizowane, ale nie wymaga interwencji',
         'P': 'POZOSTALE - nie wymaga interwencji',
     }
@@ -40,9 +40,9 @@ def load_slow():
 def format_s_field(s_field, ccc, ss):
     switcher = {
         'i': 'Numer strefy: {0}, numer wejscia (czujki): {1}'.format(ss, ccc),
-        'u': 'Numer strefy: {0}, numer u\xc5ytkownika: {1}'.format(ss, ccc),
+        'u': 'Numer strefy: {0}, numer użytkownika: {1}'.format(ss, ccc),
         's': 'Numer strefy: {0}'.format(ss),
-        'v': 'Numer u\xc5ytkownika: {0}'.format(ccc),
+        'v': 'Numer użytkownika: {0}'.format(ccc),
         'e': 'Numer strefy: {0}, numer expandera: {1}'.format(ss, ccc),
         'x': 'Zdarzenie systemowe',
     }
