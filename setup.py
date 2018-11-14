9a47b34ec904e5368b1d45c3e5936059aa8f94b0
wgebis <wgebis@gmail.com>
2018-11-14 22:28:09 +0100

    Docs

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.0.1",
     packages=['integra'],
     include_package_data=True,
+    url='https://github.com/wgebis/integra-monitor',
     author='Wojciech Gebis',
     author_email='wgebis@gmail.com',
     install_requires=[
